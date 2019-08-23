43d6671ad1a346955e91da6681d1d61585e9d228
Jeremy Newcomb <acidatropos@gmail.com>
2019-08-23 13:23:14 -0700

    Setup get expenses from database

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,16 +1,6 @@
 import uuid from "uuid";
 import database from '../firebase/firebase';
 
-// component calls action generator
-// action generator returns object
-// component dispatches object
-// redux store changes
-
-// component calls action generator
-// action generator returns function
-// component dispatches function (?)
-// function runs (has the ability to dispatch other actions and do whatever it wants)
-
 // ADD_EXPENSE
 export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
@@ -48,3 +38,28 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+});
+
+export const startSetExpenses = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+            const expenses = [];
+            snapshot.forEach((childSnapshot) => {
+                expenses.push({
+                    id: childSnapshot.key,
+                    ...childSnapshot.val()
+                });
+            });
+            dispatch(setExpenses(expenses));
+        });
+    }
+};
+
+// 1. Fetch all expense data once
+// 2. Parse that data into an array
+// 3. Dispatch SET_EXPENSES
